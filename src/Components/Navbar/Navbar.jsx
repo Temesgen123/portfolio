b2db3e0bb46ae2bd5c418beb3db4497e34ec2e4c
Temesgen123 <birechis@gmail.com>
2026-09-18 11:23:38 +0300

    Edited Mobile Menu Layout.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -69,7 +69,7 @@ const Navbar = () => {
         </li>
         <li className="mobile-social-links">
           <a
-            href="https://www.linkedin.com/in/yourprofile"
+            href="https://www.linkedin.com/in/temesgen-birechis-ab2a18319/"
             target="_blank"
             rel="noopener noreferrer"
           >
