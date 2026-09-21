ff7327640882d1d41cb5ef34376deadd7967a732
Temesgen123 <birechis@gmail.com>
2026-09-21 08:00:00 +0300

    Edited About Page.

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -75,7 +75,7 @@ const About = () => {
         <div className="technology">
           <h3>Databases</h3>
           <p>SQL Server</p>
-          <p>PosgreSQL</p>
+          <p>PostgreSQL</p>
           <p>MySQL</p>
         </div>
         <hr />
